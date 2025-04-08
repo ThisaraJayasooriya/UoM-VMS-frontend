5ec83e723f706f4ca44925f92c44d3727fc2e310
Hafsa-M1 <mushtaqfh.22@uom.lk>
2025-04-08 23:34:27 +0530

    created common pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-
+import { Routes, Route, BrowserRouter, useLocation } from "react-router-dom";
+import Home from "./pages/common/Home";
+import About from "./pages/common/About";
+import SignUp from "./pages/common/Signup"; 
+import Login from "./pages/common/Login"; 
+import HomeNavbar from "./components/home/HomeNavbar";
+import MainNavbar from "./components/home/MainNavbar";
+import ContactUs from "./pages/common/ContactUs";
+import PrivacyPolicy from "./pages/common/PrivacyPolicy";
 
 function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<MainComponent />} />
+          
 
+      
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+function MainComponent() {
+  const location = useLocation();
+  const { pathname } = location;
+
+  // Routes that should NOT display a navbar
+  const noNavbarRoutes = ["/signup", "/login"];
 
   return (
     <>
-      
+      {!noNavbarRoutes.includes(pathname) && (
+        pathname === "/" ? <HomeNavbar /> : <MainNavbar />
+      )}
+
+      <Routes>
+        <Route index element={<Home/>} />
+        <Route path="about" element={<About />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="login" element={<Login />} />
+        <Route path="contact" element={<ContactUs />} />
+        <Route path="privacy-policy" element={<PrivacyPolicy />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
